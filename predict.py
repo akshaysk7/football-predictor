c6289f21225647c3aba76455229021aac0c62c47
akshaysk7 <akshaysk2007@gmail.com>
2026-09-13 03:17:40 +0530

    day 19:v0-changed date to datetime + sorted dates in chronological order for feature engineering

--- predict.py
+++ predict.py
@@ -1,7 +1,8 @@
 import pandas as pd 
 df=pd.read_csv("E0.csv")
-print(df.head())
-print(df['MaxCAHA'].mean())
-print(df.shape)
-print(df.columns.tolist())
-print(df[['B365H', 'B365D', 'B365A']].head())
+print(df['Date'].dtype)
+df['Date'] = pd.to_datetime(df['Date'],format='%d/%m/%Y')
+print(df['Date'].dtype)
+df=df.sort_values('Date',ignore_index=True)
+
+
